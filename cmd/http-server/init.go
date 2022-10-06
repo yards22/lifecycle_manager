a699901dce365911d71e4c7d6b1183467fd0e38a
Darshan Solanki <dsolanki2702@gmail.com>
2022-10-06 12:53:43 +0530

    Add initialization of database and managers
    - Add database configs to .env file
    
    - Create App holding DB, logger and managers
    
    - Initialize database, loggers and managers in main

---
+++ cmd/http-server/init.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"database/sql"
+	"time"
+
+	sqlc "github.com/yards22/lcmanager/db/sqlc"
+	"github.com/yards22/lcmanager/internal/token_manager"
+	"github.com/yards22/lcmanager/pkg/env"
+)
+
+func initDB() (*sql.DB, error) {
+	db, err := sql.Open(env.ViperGetEnvVar("DB_DRIVER_NAME"), env.ViperGetEnvVar("DB_DATA_SOURCE_NAME"))
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func initManagers(app *App) {
+	// Initialize managers
+	tokenManager := token_manager.New(sqlc.New(app.db), time.Hour)
+	// Add to app
+	app.managers["tokenManager"] = tokenManager
+}
