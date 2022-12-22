b6ea606d182157b3ca169c59187fdf2efb08737a
Rithvik89 <innovation6178@gmail.com>
2022-12-22 18:41:00 +0530

    added logout handler and checkallownace middleware for required

--- cmd/http-server/handler.go
+++ cmd/http-server/handler.go
@@ -9,7 +9,8 @@ import (
 func initHandler(app *App, r *chi.Mux) {
 	r.Post("/sendOTP", app.handleSendOTP)
 	r.Post("/login", app.handleLogin)
-	r.Post("/poll", http.HandlerFunc(app.handleCreatePoll))
-	r.Get("/poll", http.HandlerFunc(app.handleGetPoll))
-	r.Get("/feedback", http.HandlerFunc(app.handleGetFeedback))
+	r.Delete("/logout", app.checkAllowance(http.HandlerFunc(app.handleLogout)))
+	r.Post("/poll", app.checkAllowance(http.HandlerFunc(app.handleCreatePoll)))
+	r.Get("/poll", app.checkAllowance(http.HandlerFunc(app.handleGetPoll)))
+	r.Get("/feedback", app.checkAllowance(http.HandlerFunc(app.handleGetFeedback)))
 }
