67af38035c8ae2b581755c0ef158b19dc177789a
Saichand819 <saichandgupta819@gmail.com>
2023-01-11 21:14:38 +0530

    added device state to app

--- web/src/Logic/State/AppStore.ts
+++ web/src/Logic/State/AppStore.ts
@@ -2,12 +2,37 @@ import { observable, action, makeAutoObservable } from "mobx";
 
 export default class AppStore{
     @observable currentTab: number = 0;
-    @observable currentWindowSize : number = 0;
+    @observable deviceWidth : number = 0;
     @observable selectedDate : Date = new Date();
+    @observable isPhone: boolean = false;
+    @observable isTablet: boolean = false;
+    @observable isDesktop: boolean = true;
+    @observable navigationState: number = 0;
 
     constructor(){
         makeAutoObservable(this)
     }
+
+    @action
+    setIsPhone = (isPhone: boolean) => {
+        this.isPhone = isPhone;
+        this.isTablet = false;
+        this.isDesktop = false;
+    };
+
+    @action
+    setIsTablet = (isTablet: boolean) => {
+        this.isTablet = isTablet;
+        this.isPhone = false;
+        this.isDesktop = false;
+    };
+
+    @action
+    setIsDesktop = (isDesktop: boolean) => {
+        this.isDesktop = isDesktop;
+        this.isPhone = false;
+        this.isTablet = false;
+    };
 
     @action
     SetCurrentTab = (currentTab:number)=>{
@@ -15,12 +40,17 @@ export default class AppStore{
     }
 
     @action
-    SetCurrentWindowSize = (size : number)=>{
-        this.currentWindowSize = size
+    SetCurrentDate = (date : Date) =>{
+        this.selectedDate = date
     }
 
     @action
-    SetCurrentDate = (date : Date) =>{
-        this.selectedDate = date
+    setDeviceWidth = (state: number) => {
+        this.deviceWidth = state;
+    };
+
+    @action
+    setNavigationState = (state:number)=>{
+        this.navigationState = state
     }
 }
