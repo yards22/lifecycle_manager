9d40ca0b9a7e4febf35d97b01a61a9f8d01b7863
Saichand819 <saichandgupta819@gmail.com>
2023-01-11 23:32:24 +0530

    Feedback state management beginning

--- web/src/Logic/Providers/StateProvider.ts
+++ web/src/Logic/Providers/StateProvider.ts
@@ -1,8 +1,10 @@
 import { createContext, useContext } from "react";
 import AppStore from "../State/AppStore";
+import { FeedBackStore } from "../State/FeedBack";
 
 interface IStoresContext {
   appStore: AppStore;
+  feedBackStore : FeedBackStore
 }
 
 export const StoresContext = createContext<IStoresContext>(
--- web/src/ProvidedApp.tsx
+++ web/src/ProvidedApp.tsx
@@ -3,7 +3,10 @@ import { Observer } from 'mobx-react-lite';
 import React from 'react'
 import styled from 'styled-components';
 import { StoresContext } from './Logic/Providers/StateProvider';
+import { FeedBackRepo } from './Logic/Repository/FeedBackRepo';
 import AppStore from './Logic/State/AppStore'
+import { FeedBackStore } from './Logic/State/FeedBack';
+import { Request } from './Logic/Utils/Fetch';
 
 const SProvidedApp = styled.div`
   display: flex;
@@ -13,14 +16,19 @@ interface ProvidedAppProps {
     children?: React.ReactNode;
   }
 
+const BASE_URL = "localhost:4000"
+
 function ProvidedApp(props:ProvidedAppProps) {
-  
+  const rq = new Request({});
   const appStore = new AppStore();
+  const feedBackStore = new FeedBackStore(new FeedBackRepo(BASE_URL,rq))
+
   return (
     <SProvidedApp>
       <StoresContext.Provider
         value={{
-          appStore
+          appStore,
+          feedBackStore
         }}
       >
         { 
