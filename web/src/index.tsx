7c12f21d066d76f1271605f92de05b9b828ae00f
Saichand819 <saichandgupta819@gmail.com>
2023-01-11 21:14:19 +0530

    layout fixtures

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import styled from "styled-components";
 import App from "./App";
 import ProvidedApp from "./ProvidedApp";
+
+const SIndex = styled.div`
+  display: flex;
+  justify-content: center;
+  height: calc(100vh - 60px);
+  width: 100vw;
+  margin-left: 10px;
+  margin-right: 10px;
+  margin-top: 65px;
+`;
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -9,7 +20,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ProvidedApp>
-      <App />
+      <SIndex>
+        <App />
+      </SIndex>
     </ProvidedApp>
   </React.StrictMode>
 );
