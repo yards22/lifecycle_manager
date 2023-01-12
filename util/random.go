f7e3b377f7e4763b0eeaa5c584f894883a7d5ec9
Rithvik89 <innovation6178@gmail.com>
2023-01-12 11:16:04 +0530

    standardized the code for random otp and random token generation

--- util/random.go
+++ util/random.go
@@ -1,14 +1,41 @@
 package util
 
 import (
+	"fmt"
 	"math/rand"
-	"strconv"
+	"time"
 )
 
 func GenerateRandom(n int) string {
-	var rand_str string
-	for i := 0; i < n; i++ {
-		rand_str += strconv.Itoa(rand.Intn(10))
+	rand.Seed(time.Now().Unix())
+	length := n
+
+	ran_str := make([]byte, length)
+
+	// Generating Random string
+	for i := 0; i < length; i++ {
+		ran_str[i] = byte(48 + rand.Intn(9))
 	}
-	return rand_str
+
+	// Displaying the random string
+	str := string(ran_str)
+	fmt.Println(str)
+	return str
+}
+
+func GenerateRandomToken(n int) string {
+	rand.Seed(time.Now().Unix())
+	length := n
+
+	ran_str := make([]byte, length)
+
+	// Generating Random string
+	for i := 0; i < length; i++ {
+		ran_str[i] = byte(65 + rand.Intn(25))
+	}
+
+	// Displaying the random string
+	str := string(ran_str)
+	fmt.Println(str)
+	return str
 }
