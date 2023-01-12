81a87573fc2a6db94acd0ab98a57d9e7b430c983
sunkoshi <itsmehs07@gmail.com>
2023-01-12 16:24:22 +0530

    add missing currentWindowSize in appstore

--- web/src/Logic/State/AppStore.ts
+++ web/src/Logic/State/AppStore.ts
@@ -1,56 +1,57 @@
 import { observable, action, makeAutoObservable } from "mobx";
 
-export default class AppStore{
-    @observable currentTab: number = 0;
-    @observable deviceWidth : number = 0;
-    @observable selectedDate : Date = new Date();
-    @observable isPhone: boolean = false;
-    @observable isTablet: boolean = false;
-    @observable isDesktop: boolean = true;
-    @observable navigationState: number = 0;
+export default class AppStore {
+  @observable currentTab: number = 0;
+  @observable deviceWidth: number = 0;
+  @observable selectedDate: Date = new Date();
+  @observable isPhone: boolean = false;
+  @observable isTablet: boolean = false;
+  @observable isDesktop: boolean = true;
+  @observable navigationState: number = 0;
+  @observable currentWindowSize: number = window.innerWidth;
 
-    constructor(){
-        makeAutoObservable(this)
-    }
+  constructor() {
+    makeAutoObservable(this);
+  }
 
-    @action
-    setIsPhone = (isPhone: boolean) => {
-        this.isPhone = isPhone;
-        this.isTablet = false;
-        this.isDesktop = false;
-    };
+  @action
+  setIsPhone = (isPhone: boolean) => {
+    this.isPhone = isPhone;
+    this.isTablet = false;
+    this.isDesktop = false;
+  };
 
-    @action
-    setIsTablet = (isTablet: boolean) => {
-        this.isTablet = isTablet;
-        this.isPhone = false;
-        this.isDesktop = false;
-    };
+  @action
+  setIsTablet = (isTablet: boolean) => {
+    this.isTablet = isTablet;
+    this.isPhone = false;
+    this.isDesktop = false;
+  };
 
-    @action
-    setIsDesktop = (isDesktop: boolean) => {
-        this.isDesktop = isDesktop;
-        this.isPhone = false;
-        this.isTablet = false;
-    };
+  @action
+  setIsDesktop = (isDesktop: boolean) => {
+    this.isDesktop = isDesktop;
+    this.isPhone = false;
+    this.isTablet = false;
+  };
 
-    @action
-    SetCurrentTab = (currentTab:number)=>{
-        this.currentTab = currentTab;
-    }
+  @action
+  SetCurrentTab = (currentTab: number) => {
+    this.currentTab = currentTab;
+  };
 
-    @action
-    SetCurrentDate = (date : Date) =>{
-        this.selectedDate = date
-    }
+  @action
+  SetCurrentDate = (date: Date) => {
+    this.selectedDate = date;
+  };
 
-    @action
-    setDeviceWidth = (state: number) => {
-        this.deviceWidth = state;
-    };
+  @action
+  setDeviceWidth = (state: number) => {
+    this.deviceWidth = state;
+  };
 
-    @action
-    setNavigationState = (state:number)=>{
-        this.navigationState = state
-    }
+  @action
+  setNavigationState = (state: number) => {
+    this.navigationState = state;
+  };
 }
