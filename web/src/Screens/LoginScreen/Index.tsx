0cc2188ef02e555913babc03fa59c90800aa4ca9
Saichand819 <saichandgupta819@gmail.com>
2023-01-12 23:23:20 +0530

    login screen update with email input

--- web/src/Screens/LoginScreen/Index.tsx
+++ web/src/Screens/LoginScreen/Index.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import { Button, Card, Input } from '@mantine/core'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import { useStores } from '../../Logic/Providers/StateProvider'
 
 const SLoginScreenIndex = styled.div`
     display: flex;
@@ -11,8 +13,35 @@ const SLoginScreenIndex = styled.div`
 `
 
 function LoginScreenIndex() {
+  const stores = useStores();
+  const [mailId,setMailId] = useState("")
+
+  function handleMailIdChange(e:any){
+    setMailId(e.target.value)
+  }
+
+  function handleSendAuthOTP(){
+    stores.authStore.SendAuthOTP(mailId)
+  }
+
   return (
-    <SLoginScreenIndex>LoginScreenIndex</SLoginScreenIndex>
+    <SLoginScreenIndex>
+       <Card
+         shadow="sm" 
+         p="lg" 
+         radius="md" 
+         withBorder
+         style={{ display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column" }}
+       >
+          <Input 
+              placeholder="email" 
+              onChange={handleMailIdChange} 
+              style={{minWidth : "250px", marginBottom:"15px"}} 
+              type={"email"}
+           />
+           <Button onClick={handleSendAuthOTP}>Send OTP</Button>
+       </Card>
+    </SLoginScreenIndex>
   )
 }
 
