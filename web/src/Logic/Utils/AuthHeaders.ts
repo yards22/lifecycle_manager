edfcef317febec6f140f292924cc0d5945361e81
Saichand819 <saichandgupta819@gmail.com>
2023-01-13 22:44:57 +0530

    Auth header with token util added

--- web/src/Logic/Utils/AuthHeaders.ts
+++ web/src/Logic/Utils/AuthHeaders.ts
@@ -2,4 +2,11 @@ export function AuthHeadersWithoutToken() {
     return {
         "Content-Type": "application/json"
     };
-  }
+}
+
+export function AuthHeadersWithToken(token:string){
+    return{
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+    }
+}
