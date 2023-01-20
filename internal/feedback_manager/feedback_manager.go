f5618bae2cdd77d163b40469c6226a4c379a4b1c
Rithvik89 <innovation6178@gmail.com>
2023-01-21 00:44:59 +0530

    feedback GET and POST(comments) taken care in backend

--- internal/feedback_manager/feedback_manager.go
+++ internal/feedback_manager/feedback_manager.go
@@ -5,10 +5,17 @@ import (
 	"fmt"
 
 	sqlc "github.com/yards22/lcmanager/db/sqlc"
+	"gopkg.in/guregu/null.v4"
 )
 
 type FeedbackManager struct {
 	querier sqlc.Querier
+}
+
+type UpdateCommentsParams struct {
+	Status     bool        `json:"status"`
+	Comment    null.String `json:"comment"`
+	FeedbackID int64       `json:"feedback_id"`
 }
 
 func New(querier sqlc.Querier) *FeedbackManager {
@@ -24,5 +31,21 @@ func (fm *FeedbackManager) GetFeedback(ctx context.Context) []*sqlc.Feedback {
 	}
 
 	return feedback
+
+}
+
+func (fm *FeedbackManager) PostCommentFeedback(ctx context.Context, arg UpdateCommentsParams) {
+
+	err := fm.querier.UpdateComments(ctx, sqlc.UpdateCommentsParams{
+		Status:     arg.Status,
+		Comment:    arg.Comment,
+		FeedbackID: arg.FeedbackID,
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return
 
 }
