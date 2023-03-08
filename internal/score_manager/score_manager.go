988daf9ceffe685044d5afb1bee97b44e028a1bd
Rithvik89 <innovation6178@gmail.com>
2023-03-08 12:24:12 +0530

    commentry call done in score_manager

--- internal/score_manager/score_manager.go
+++ internal/score_manager/score_manager.go
@@ -8,17 +8,13 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/streadway/amqp"
+	"github.com/yards22/lcmanager/internal/entities"
 )
 
-type Details struct {
-	MatchId string `json:"match_id"`
-	OwnerId int    `json:"owner_id"`
-}
-
 type Summary struct {
-	MatchId  string  `json:"match_id"`
-	DataType string  `json:"data_type"`
-	Score    Details `json:"Score"`
+	MatchId  string             `json:"match_id"`
+	DataType string             `json:"data_type"`
+	Score    entities.ScoreItem `json:"Score"`
 }
 
 type ScoreManager struct {
@@ -59,11 +55,11 @@ func (sm *ScoreManager) Close() {
 }
 
 func (sm *ScoreManager) process(d amqp.Delivery) {
-	var s Details
+	var s entities.ScoreItem
 	json.Unmarshal(d.Body, &s)
 	r := Summary{
-		MatchId:  "match_2",
-		DataType: "match_2_raw",
+		MatchId:  s.MatchId,
+		DataType: s.MatchId + "_raw",
 		Score:    s,
 	}
 	fmt.Println(s)
@@ -79,4 +75,53 @@ func (sm *ScoreManager) process(d amqp.Delivery) {
 	if err != nil {
 		panic(fmt.Sprintf("failed to put Record to DynamoDB, %v", err))
 	}
+}
+
+func (sm *ScoreManager) generateCommentry(raw entities.ScoreItem) entities.CommentryF {
+	// create commentry and send it back to process function ..
+	if raw.WicketDetails.IsWicket {
+		r := entities.Wicket{
+			Overs:             raw.InningsDetails.Overs,
+			Balls:             raw.InningsDetails.Balls,
+			WicketType:        raw.WicketDetails.WicketType,
+			StrikerBatsman:    raw.PlayersInAction.StrikerBatsman,
+			NonStrikerBatsman: raw.PlayersInAction.NonStrikerBatsman,
+			Bowler:            raw.PlayersInAction.Bowler,
+			RunsScored:        raw.RunsDetails.RunsScored,
+			WagonDirection:    raw.RunsDetails.WagonDirection,
+			IsFielder:         raw.WicketDetails.IsFielder,
+			FieldedBy:         raw.WicketDetails.FieldedBy,
+			IsExtra:           raw.ExtraDetails.IsExtra,
+			ExtraType:         raw.ExtraDetails.ExtraType,
+		}
+		return r.WicketC()
+	}
+	if raw.RunsDetails.RunsScored != 0 {
+		r := entities.Runs{
+			Overs:             raw.InningsDetails.Overs,
+			Balls:             raw.InningsDetails.Balls,
+			RunsScored:        raw.RunsDetails.RunsScored,
+			StrikerBatsman:    raw.PlayersInAction.StrikerBatsman,
+			NonStrikerBatsman: raw.PlayersInAction.NonStrikerBatsman,
+			Bowler:            raw.PlayersInAction.Bowler,
+			IsBoundary:        raw.RunsDetails.IsBoundary,
+			BoundaryType:      raw.RunsDetails.BoundaryType,
+			WagonDirection:    raw.RunsDetails.WagonDirection,
+			IsExtra:           raw.ExtraDetails.IsExtra,
+			ExtraType:         raw.ExtraDetails.ExtraType,
+		}
+		return r.RunsC()
+	}
+	if raw.ExtraDetails.IsExtra {
+		r := entities.Extra{
+			Overs:             raw.InningsDetails.Overs,
+			Balls:             raw.InningsDetails.Balls,
+			ExtraType:         raw.ExtraDetails.ExtraType,
+			StrikerBatsman:    raw.PlayersInAction.StrikerBatsman,
+			NonStrikerBatsman: raw.PlayersInAction.NonStrikerBatsman,
+			Bowler:            raw.PlayersInAction.Bowler,
+		}
+		return r.ExtraC()
+	}
+	return entities.CommentryF{}
 }
