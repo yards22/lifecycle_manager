1f07c6f6f33972bd2166f91fdafcc7fe83fdad16
Rithvik89 <innovation6178@gmail.com>
2023-03-22 13:06:52 +0530

    logout feature added in topBar

--- web/src/Organs/Navbar/TopBar/TopBarMobile.tsx
+++ web/src/Organs/Navbar/TopBar/TopBarMobile.tsx
@@ -1,14 +1,17 @@
 import { Burger, Title, useMantineTheme } from '@mantine/core';
 import { Observer } from 'mobx-react-lite';
 import React, { useState } from 'react'
-import { Search } from 'react-feather';
+import { LogOut, Search } from 'react-feather';
 import { useStores } from '../../../Logic/Providers/StateProvider';
 import NavBarMobile from '../NavbarMobile/Index';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 function TopBarMobile() {
     const stores = useStores();
     const mantineTheme = useMantineTheme();
     const [isNavBarOpened, setIsNavBarOpened] = useState(false)
+    const navigate = useNavigate();
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
     
     return (
         <Observer>
@@ -26,22 +29,31 @@ function TopBarMobile() {
                 height: "60px",
                 width: "100%",
                 display: "flex",
-                justifyContent: "center",
+                justifyContent: "space-between",
                 alignItems: "center",
             }}
             >
             <Burger 
                 opened={isNavBarOpened}
                 color = {'white'}
-                style = {{
-                    position : "absolute",
-                    left : "13px"
-                }}
                 onClick = {()=>{setIsNavBarOpened(!isNavBarOpened)}}
             />
             <Title color={"white"} order={5}>
                 Admin 22 Yards 
             </Title>
+            <LogOut
+                color = {'white'}
+                onClick = {()=>{
+                    console.log("h")
+                    stores.authStore.PerformLogout().then(
+                        ()=>{
+                            navigate( "/login")
+                        })
+                        .catch(e=>{
+                            throw e
+                        })
+                }}
+             />
             { isNavBarOpened && <NavBarMobile setIsNavBarOpened={setIsNavBarOpened}/> }
             </div>
         )
