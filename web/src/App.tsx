0ab954e9deea82951583f2ec3028e76722e0cb96
Rithvik89 <innovation6178@gmail.com>
2023-03-22 12:52:42 +0530

    added protected route and brought TopBar & BottomBar inside router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,5 @@
-import { AppShell, Burger,  Header, MediaQuery, Navbar, Text, useMantineTheme } from "@mantine/core";
 import { Observer } from "mobx-react-lite";
-import React, { useEffect, useRef, useState } from "react";
+import  { useEffect } from "react";
 import { useStores } from "./Logic/Providers/StateProvider";
 import FeedBackScreenIndex from "./Screens/FeedbackScreen/Index";
 import PollsScreenIndex from "./Screens/PollsScreen/Index";
@@ -10,16 +9,8 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import TopBar from "./Organs/Navbar/TopBar/TopBar";
 import BottomBar from "./Organs/Navbar/BottomBar";
 import LoginScreenIndex from "./Screens/LoginScreen/Index";
+import ProtectedRoutes from "./ProtectedRoutes";
 
-const STabs = styled.div`
-  height: 50px;
-  width: 100%;
-  border-radius: 5px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-`
 
 const SApp = styled.section`
   width: 100%;
@@ -32,10 +23,7 @@ const SApp = styled.section`
 const TOKEN_KEY = "token";
 
 function App() {
-  const [opened, setOpened] = useState(false);
-  const theme = useMantineTheme();
   const store = useStores();
-  const divRef:any = useRef(null);
 
   useEffect(()=>{
     handleScreenSizeChange();
@@ -49,7 +37,6 @@ function App() {
   },[])
 
   const handleScreenSizeChange = ()=>{
-    //  store.appStore.SetCurrentWindowSize(divRef.current.clientWidth)
     store.appStore.setDeviceWidth(window.innerWidth);
       if (window.innerWidth <= 700) {
         store.appStore.setIsPhone(true);
@@ -68,19 +55,17 @@ function App() {
           const {appStore,authStore} = store;
           return(
             <Router>
+              <SApp>
               { authStore.isUserLoggedIn && appStore.isPhone && <BottomBar/>}
               { authStore.isUserLoggedIn && <TopBar/>}
-              <SApp>
                  <Routes>
-                    { !authStore.isUserLoggedIn && <Route path="/" element={<LoginScreenIndex/>} />}
-                    {  authStore.isUserLoggedIn &&
-                      <>
+                  <Route path="/login" element={<LoginScreenIndex/>}/>
+                      <Route element={<ProtectedRoutes/>}>
                         <Route path="/feedback" element={<FeedBackScreenIndex/>}/>
                         <Route path="/polls" element={<PollsScreenIndex/>}/>
                         <Route path="/stories" element={<StoriesScreenIndex/>}/>
                         <Route path="*" element={<Navigate to={"/feedback"}/>} />
-                      </>
-                    }
+                      </Route>
                  </Routes>
               </SApp>
             </Router>
